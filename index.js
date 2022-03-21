341cca1991d4433ad1ce3ee788989e028f0cd9a5
malbec <evelyn.ec.dc@gmail.com>
2022-03-21 19:42:49 -0300

    primer desafio

---
+++ index.js
@@ -0,0 +1,9 @@
+let textoA = 'Bienvenida a Baraco';
+let nombreIngresado= prompt ('Ingrese su nombre');
+console.log ('Ingrese su nombre');
+
+let unir= textoA + nombreIngresado
+
+if primeracompra =true;
+if (primeracompra) { alert ('15% off');}
+if (false) {("no vas a ver este mensaje")}
